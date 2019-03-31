3c059f6079451896ca614e3a12b3c0a68ca39cb4
Dmitry <usachev2012@gmail.com>
2019-03-31 22:04:52 +0400

    It i our first commit

---
+++ scripts/scrip1.js
@@ -0,0 +1,27 @@
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+
+let arr = ['plum.png', 'orange.jpg'];
+
+console.log(arr[1]);
+
+// alert('Timi siski');
+
+//let answer = confirm('Are you sure?');
+//console.log(answer);
+
+//let answer = +prompt('Are you 18?', 'Yes');
+//console.log(answer);
+//console.log('siski' + 'siski');
+
+let incr = 10, decr =10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(incr);
