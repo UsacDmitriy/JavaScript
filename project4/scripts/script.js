6d72dc3c70c3b701672313eb54bd057b8f0b7db1
Dmitry <usachev2012@gmail.com>
2019-04-08 16:03:54 +0400

    Work цшер вщс№2

--- project4/scripts/script.js
+++ project4/scripts/script.js
@@ -2,9 +2,42 @@ let box = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByClassName('circle'),
     heart = document.querySelectorAll('.heart');
+    oneHeart = document.querySelector('.heart');
+    wrapper = document.querySelector('.wrapper');
 
-console.log(box);
-console.log(btn);
-console.log(btn[0]);
-console.log(circle[2]);
-console.log(heart);
+  box.style.backgroundColor = 'blue';
+  btn[2].style.borderRadius = '100%';
+  circle[0].style.backgroundColor = "red";
+  circle[1].style.backgroundColor = "yellow";
+  circle[2].style.backgroundColor = "green";
+
+ // for (let i = 0; i < heart.length; i++) {
+ //    heart[i].style.backgroundColor = 'blue';
+ // }
+
+ /* heart.forEach(function(item, i, hearts){
+      item.style.backgroundColor = 'black';
+  }); */
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Hi guys');
+
+div.classList.add('black');
+
+//document.body.appendChild(div);
+//wrapper.appendChild(div);
+
+//div.innerHTML = '<p>Hello world!</p>';
+div.textContent = "hello people"
+
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+wrapper.removeChild(heart[1]);
+
+document.body.replaceChild(btn[2], circle[1]);
+
+
+
+console.log(div);
+
+
