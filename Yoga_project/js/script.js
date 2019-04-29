d182b8c6b9ed6a8e83bf01f7c0e4c993f66668bc
Dmitry <usachev2012@gmail.com>
2019-04-29 15:01:22 +0400

    Timer on project

--- Yoga_project/js/script.js
+++ Yoga_project/js/script.js
@@ -37,5 +37,46 @@ window.addEventListener('DOMContentLoaded', function() {
 
     });
 
+    //Timer
+
+    let deadLine = '2019-04-30';
+
+    function getTimeRemaining (endTime) {
+        let t = Date.parse(deadLine) - Date.parse(new Date()),    
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+        
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds':seconds
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= endTime) {
+                clearInterval(timeInterval);
+            }
+        }
+        }
+    
+
+    setClock('timer', deadLine);
 
 });
+
+
