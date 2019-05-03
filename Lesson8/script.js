7df2056c7f6387bb85645b04b1ab30d6121add86
Dmitry <usachev2012@gmail.com>
2019-05-03 21:53:01 +0400

    OOP and THIS

--- Lesson8/script.js
+++ Lesson8/script.js
@@ -1,7 +1,14 @@
-function User(name, id) {
+/*function User(name, id) {
     this.name = name;
     this.id = id;
     this.human = true;
+    this.hello = function() {
+        console.log('Hello! ' + this.name);
+    };
+}
+
+User.prototype.exit = function (name) {
+    console.log('User' + this.name + 'goes out');
 }
 
 let ivan = new User('Ivan', 25);
@@ -9,3 +16,35 @@ let alex = new User('Alex', 20);
 
 console.log(ivan);
 console.log(alex);
+
+ivan.exit(); */
+
+/*function showThis(a, b) {
+    console.log(this);
+    function sum() {
+        console.log(this);
+        return a + b;
+    }
+    console.log(sum());
+}
+
+showThis(4, 5);
+showThis(4, 36);*/
+let obj = {
+    a: 20,
+    b: 30,
+    sum: function () {
+        console.log(this);
+        function shout() {
+            console.log(this);
+        }
+        shout();
+    }
+};
+
+obj.sum();
+
+// 1) Invoke function - window/undefined
+// 2) 
+
+
