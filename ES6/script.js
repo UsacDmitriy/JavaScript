95166fd42afec11e46dd93625974485e9ebfbcc1
Dmitry <usachev2012@gmail.com>
2019-05-06 22:09:53 +0400

    ES6

--- ES6/script.js
+++ ES6/script.js
@@ -3,3 +3,22 @@ let name = 'Dmitry',
     mail = 'usachev2012@gmail.com';
 
 document.write(`Age of ${name} is ${age}. His email is ${mail}`);
+
+function makeArray() {
+    var items = [];
+
+    for (let i =0; i < 10; i++) {
+        var item = function() {
+            console.log(i);
+        };
+        items.push(item);
+    }
+
+    return items;
+}
+
+var arr = makeArray();
+
+arr[1]();
+arr[3]();
+arr[7]();
