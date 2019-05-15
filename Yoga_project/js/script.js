57ece3f914abea20eee95c77986c2ece43d8558f
Dmitry <usachev2012@gmail.com>
2019-05-15 22:19:21 +0400

    Yoga project

--- Yoga_project/js/script.js
+++ Yoga_project/js/script.js
@@ -161,6 +161,44 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
     });
+
+    let formContact = document.querySelector('#form'),
+    inputContact = formContact.getElementsByTagName('input');
+
+    formContact.addEventListener('submit', function() {
+        event.preventDefault();
+        formContact.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        
+        let formData = new FormData(formContact);
+        let  obj = {};
+
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.readyState == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for ( let i = 0; i < inputContact.length; i++) {
+            inputContact[i].value = '' ;
+        }
+    });
+
+
 });
 
 
